ae350695ed4468618c3c5d0a39ab963eb6f4a2df
Marlon Santana dos Santos <marlonfms2012@gmail.com>
2021-10-16 10:59:23 -0300

    Exibindo lista de pokemons

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+  <div v-for="(poke, index) in pokemons" :key="index">
+    <pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
+  </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import pokemon from './components/Pokemon.vue';
 
 export default {
   name: 'App',
+
+  data(){
+    return{
+      pokemons: []
+    }
+  },
+
+  created: function(){
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res =>{
+      console.log(res.data.results);
+      this.pokemons = res.data.results;
+    }).catch(err =>{
+      console.log(err);
+    })
+  },
   components: {
-    HelloWorld
+    pokemon
   }
 }
 </script>
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+      <h1>{{num}} {{nameUpper}}</h1>
+      <small>{{url}}</small>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+
+    created: function(){
+        axios.get(this.url).then(res =>{
+            this.pokemon.type = res.data.types[0].type.name;
+            this.pokemon.front = res.data.sprites.front_default;
+            this.pokemon.back = res.data.sprites.back_default;
+            console.log(this.pokemon.type)
+            console.log(this.pokemon.front)
+        })
+    },
+
+    data(){
+        return{
+           pokemon: {} 
+        }
+        
+    },
+
+    props: {
+        name: String,
+        url: String,
+        num: Number
+    },
+    computed:{
+        nameUpper(){
+            //var newName = name[0].toUpperCase() + name.slice(1);
+            return this.name[0].toUpperCase() + this.name.slice(1);
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
